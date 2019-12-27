53e61bb603a3c55fdf9352347e5b52e1e6dbb8cc
Ignacio Roncero Bazarra <ironcero@gmail.com>
2019-12-28 00:30:51 +0100

    Jenkins: user and plugins

--- startup.sh
+++ startup.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 mkdir liferay/deploy
+mkdir -p elastic/data
+mkdir -p jenkins/home
+mkdir fakemail
 chown 1000:1000 liferay/deploy
+chown -R 1000:1000 elastic
+chown -R 1000:1000 jenkins
+chown -R 1000:1000 fakemail
 docker-compose up
